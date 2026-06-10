d1fa74ab3cb45ca0340538b633e46cab78d0f9c9
Felix Soares <felix.soares.ferreira@gmail.com>
2026-06-09 21:49:36 -0300

    add text align center in error view

--- app/src/main/java/com/mobile/felix/musicapp/core/presentation/ErrorContentView.kt
+++ app/src/main/java/com/mobile/felix/musicapp/core/presentation/ErrorContentView.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,6 +26,7 @@ fun ErrorContentView(
     ) {
         Text(
             text = message,
+            textAlign = TextAlign.Center,
             modifier = Modifier.padding(end = 16.dp, start = 16.dp)
         )
 
